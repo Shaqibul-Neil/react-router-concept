0a8b89acb56de6fec7cbe5e555252df8bb4bb6a9
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-09-28 19:50:41 +0600

    Understand The Concept of Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return;
+  return <h1>App</h1>;
 }
 
 export default App;
---
+++ src/components/Mobiles/Mobiles.jsx
@@ -0,0 +1,9 @@
+const Mobiles = () => {
+  return (
+    <div>
+      <h2>This is mobile</h2>
+    </div>
+  );
+};
+
+export default Mobiles;
